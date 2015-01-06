811845f3d8cd49dc37e6e5f3dfee0e9edb4cef28
Toshiaki Toyama <ttoyama@knownstylenolife.com>
2015-01-06 23:12:59 +0900

    Add kdd dataset

---
+++ script/repo/kdd_cup_1999-env.sh
@@ -0,0 +1,5 @@
+# KDD CUP 1999 test data set 
+#   This database contains a standard set of data to be audited, which includes a wide variety of intrusions simulated in a military network environment.
+#   http://kdd.ics.uci.edu/databases/kddcup99/kddcup99.html
+NAME=kdd_cup_1999
+URL=http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data_10_percent.gz
---
+++ script/repo/kdd_cup_2012-env.sh
@@ -0,0 +1,6 @@
+# KDD CUP 2012 test data set 
+#   Predict the click-through rate of ads given the query and user information.
+#   http://www.kddcup2012.org/c/kddcup2012-track2
+NAME=kdd_cup_2012
+URL=http://www.kddcup2012.org/c/kddcup2012-track2/download/test.7z
+LOGIN=required
